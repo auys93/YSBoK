0987d6ede1b3361f869c818b1801d01c101c323d
auys93 <auys93@gmail.com>
2024-08-07 12:48:04 +0000

    Include Unique IP Analysis

--- logparser.py
+++ logparser.py
@@ -4,6 +4,9 @@ import json
 # Folder containing log files (replace with your folder path)
 log_folder = "/workspaces/YSBoK/Logs"
 
+# Standard HTTP request methods
+standard_methods = {"GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH"}
+
 # def process_log_entry(log_entry):
 #     try:
 #         log_data = json.loads(log_entry)
@@ -17,16 +20,20 @@ log_folder = "/workspaces/YSBoK/Logs"
 #     except json.JSONDecodeError:
 #         print("Error: Invalid log entry format")
 
+#function to perform all analysis
 def process_log_entry(log_entry, unique_ips):
     try:
         log_data = json.loads(log_entry)
-        print(json.dumps(log_data, indent=4))
-        with open("/workspaces/YSBoK/Output/output.txt", "a") as f:
-            f.write(json.dumps(log_data, indent=4) + "\n")
+        # print(json.dumps(log_data, indent=4))
+        # with open("/workspaces/YSBoK/Output/output.txt", "a") as f:
+        #     f.write(json.dumps(log_data, indent=4) + "\n")
+        #Code line to get all Client IPs to and to get the unique ones making the requests
         client_ip = log_data.get("ClientIP")
-        if client_ip:
+        request_bytes = log_data.get("ClientRequestBytes", 0)
+        request_method = log_data.get("ClientRequestMethod", "").upper()
+        if client_ip and request_bytes != 0 and request_method in standard_methods:
             unique_ips.add(client_ip)
-
+        
     except json.JSONDecodeError:
         print("Error: Invalid log entry format")
         print(f"Raw Log Entry: {log_entry}")
@@ -52,9 +59,13 @@ def process_log_files(folder_path):
                 for entry in log_entries:
                     process_log_entry(entry, unique_ips)
                     total_entries += 1  # Increment the count
-                    print("-" * 40)
+                    #print("-" * 40)
+    print("----Log Analyst Report----")
     print(f"Total log entries: {total_entries}")
-    print(f"Total unique client IP addresses: {len(unique_ips)}")
+    print(f"Total unique client IP addresses making requests: {len(unique_ips)}")
+    print("Unique IP addresses:")
+    for ip in unique_ips:
+        print(ip)
 
 if __name__ == "__main__":
     process_log_files(log_folder)
